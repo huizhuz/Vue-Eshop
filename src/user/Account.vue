3e9d23bdca832693fddcf5ef9b9ec3598c4bc29c
huizhuz <huizhuz@outlook.com>
2019-11-20 15:18:47 -0500

    Update account page

--- src/user/Account.vue
+++ src/user/Account.vue
@@ -1,13 +1,27 @@
 <template>
-    <p>{{userBalance}}</p>
+	<div class="container">
+		<h2>Shopping cart</h2>
+		<line-break></line-break>
+        <p>Username: {{userName}}</p>
+		<p>Your balance: ${{userBalance}}</p>
+	</div>
 </template>
 
 <script>
-export default {
-    computed: {
-        userBalance() {
-            return this.$store.state.user.userBalance;
-        }
-    }
-}
+	export default {
+		computed: {
+			userBalance() {
+				return Math.floor(this.$store.state.user.userBalance);
+            },
+            userName(){
+                return this.$store.state.user.username;
+            }
+		}
+	};
 </script>
+
+<style scoped>
+	.container {
+		width: 80vw;
+	}
+</style>
